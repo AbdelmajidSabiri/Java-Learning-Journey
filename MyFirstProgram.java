cd850dc36735a45342ae074c8c690999af3734c9
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-09-29 15:39:42 +0100

    Create files

---
+++ MyFirstProgram.java
@@ -0,0 +1,8 @@
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        System.out.println("I love my mother");
+        System.out.println("      And");
+        System.out.println("   My father");
+    }
+}
