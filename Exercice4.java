3bd4cfbcfd3e9bddd6b336a879f4f887456b2bea
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-10-05 09:38:55 +0100

    try switch statement

---
+++ Exercice4.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Exercice4 {
+
+ public static void main(String[] args) {
+  
+  Scanner scanner = new Scanner(System.in);
+  System.out.print("Enter day name : ");
+  String day = scanner.nextLine();
+  
+  switch(day) {
+   case "Sunday": System.out.println("It is Sunday!");
+   break;
+   case "Monday": System.out.println("It is Monday!");
+   break;
+   case "Tuesday": System.out.println("It is Tuesday!");
+   break;
+   case "Wednesday": System.out.println("It is Wednesday!");
+   break;
+   case "Thursday": System.out.println("It is Thursday!");
+   break;
+   case "Friday": System.out.println("It is Friday!");
+   break;
+   case "Saturday": System.out.println("It is Saturday!");
+   break;
+   default: System.out.println("That is not a day!");
+  }
+    
+ }
+}
