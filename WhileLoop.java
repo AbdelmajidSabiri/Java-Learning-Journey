12b25dbe20a3caa0f47d8114b2587ef4070e6811
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-10-05 09:42:08 +0100

    Try while loop

---
+++ WhileLoop.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class WhileLoop {
+
+ public static void main(String[] args) {
+  
+
+  Scanner scanner = new Scanner(System.in);
+  String name = "";
+  
+  // check if name is empty
+  while(name.isBlank()) {
+   System.out.print("Enter your name: ");
+   name = scanner.nextLine();
+  }
+  
+  System.out.println("Hello "+name);
+   
+ }
+}
