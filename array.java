49b01e32d26b6833ae9fb4df435f754352bd1549
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-10-05 10:37:49 +0100

    Try array

---
+++ array.java
@@ -0,0 +1,17 @@
+public class array {
+
+	public static void main(String[] args) {
+			
+		
+		String[] cars = new String[3];
+		
+		cars[0] = "Camaro";
+		cars[1] = "Corvette";
+		cars[2] = "Tesla";
+			
+		for(int i=0; i<cars.length; i++) {
+			System.out.println(cars[i]);
+		}
+	
+	}
+}
