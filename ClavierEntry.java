09401a99809e182a34b3e68a8e802acfd001ed6e
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-10-13 21:57:16 +0100

    Test clavier class

---
+++ ClavierEntry.java
@@ -0,0 +1,13 @@
+package Package;
+
+public class ClavierEntry {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Enter a number : ");
+		int a = Clavier.lireInt();
+		System.out.println("Number entred is : "+a);
+		
+	}
+
+}
