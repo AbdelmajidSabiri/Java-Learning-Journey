ab3e8dfce32a0745a5dc9c2755d5a9bad22e5ef7
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-10-17 20:14:10 +0100

    Methodes exercice

---
+++ Methodes.java
@@ -0,0 +1,16 @@
+package Package;
+
+public class Methodes {
+
+	public static double somme (int a,int b) {
+		return a+b;
+	}
+		
+	
+	public static void main(String[] args) {
+
+		System.out.println(somme(7,2));
+
+	}
+
+}
